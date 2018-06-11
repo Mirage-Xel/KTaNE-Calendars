bdabc86f358c465b3e472fe239ccbcfa4a51da3f
AAces <djp0908@yahoo.com>
2018-06-10 23:08:20 -0600

    Added janurary

--- Assets/calendar.cs
+++ Assets/calendar.cs
@@ -1,16 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using Newtonsoft.Json;
+using KMHelper;
+using System;
+using System.Threading;
+using Random = UnityEngine.Random;
 using UnityEngine;
 
 public class calendar : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private static int _moduleIdCounter = 1;
+    public KMAudio newAudio;
+    public KMBombModule module;
+    public KMBombInfo info;
+    private int _moduleId = 0;
+    private bool _isSolved = false, _lightsOn = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        _moduleId = _moduleIdCounter++;
+        module.OnActivate += Activate;
+    }
+
+    private void Awake()
+    {
+        //handle button presses
+    }
+
+    void Activate()
+    {
+        Init();
+        _lightsOn = true;
+    }
+
+    void Init()
+    {
+
+    }
 }
